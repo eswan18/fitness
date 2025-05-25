a2c32cd6f9c9dc025e657866aea27b02b34b61f0
Ethan Swan <ethanpswan@gmail.com>
2025-05-25 18:16:49 -0500

    Reorganize queries.

--- dashboard/src/AllTimeStatsPanel.tsx
+++ dashboard/src/AllTimeStatsPanel.tsx
@@ -3,22 +3,12 @@ import { SummaryBox } from "./components/SummaryBox";
 import { fetchTotalMileage, fetchTotalSeconds } from "./lib/api";
 
 export function AllTimeStatsPanel() {
-  const metricsQueryResult = useQuery({
-    queryKey: ["miles", "total"],
-    queryFn: fetchTotalMileage,
-  });
-  const secondsQueryResult = useQuery({
-    queryKey: ["seconds", "total"],
-    queryFn: fetchTotalSeconds,
-  });
-  if (metricsQueryResult.isPending || secondsQueryResult.isPending) {
+  const { miles, seconds, isPending, error } = useAllTimeStats();
+  if (isPending) {
     return <p>Loading...</p>;
   }
-  if (metricsQueryResult.error) {
-    return <p>Error: {metricsQueryResult.error.message}</p>;
-  }
-  if (secondsQueryResult.error) {
-    return <p>Error: {secondsQueryResult.error.message}</p>;
+  if (error) {
+    return <p>Error: {error.message}</p>;
   }
   return (
     <div className="flex flex-col gap-y-4">
@@ -26,17 +16,70 @@ export function AllTimeStatsPanel() {
       <div className="flex flex-row w-full gap-x-4">
         <SummaryBox
           title="Miles"
-          value={Math.round(metricsQueryResult.data).toLocaleString()}
+          value={Math.round(miles).toLocaleString()}
           size="md"
           className="flex-grow-1"
         />
         <SummaryBox
           title="Minutes"
-          value={Math.round(secondsQueryResult.data / 60).toLocaleString()}
+          value={Math.round(seconds / 60).toLocaleString()}
           size="md"
           className="flex-grow-1"
         />
       </div>
     </div>
   );
+}
+
+type AllTimeStatsResult = {
+  miles: undefined;
+  seconds: undefined;
+  isPending: true;
+  error: null;
+} | {
+  miles: number;
+  seconds: number;
+  isPending: false;
+  error: null;
+} | {
+  miles: undefined;
+  seconds: undefined;
+  isPending: false
+  error: Error;
+};
+
+function useAllTimeStats(): AllTimeStatsResult {
+  const metricsQueryResult = useQuery({
+    queryKey: ["miles", "total"],
+    queryFn: () => fetchTotalMileage(),
+  });
+  const secondsQueryResult = useQuery({
+    queryKey: ["seconds", "total"],
+    queryFn: () => fetchTotalSeconds(),
+  });
+  const isPending = metricsQueryResult.isPending ||
+    secondsQueryResult.isPending;
+  const error = metricsQueryResult.error ?? secondsQueryResult.error;
+  if (isPending) {
+    return {
+      miles: undefined,
+      seconds: undefined,
+      isPending: true,
+      error: null,
+    };
+  }
+  if (error) {
+    return {
+      miles: undefined,
+      seconds: undefined,
+      isPending: false,
+      error,
+    };
+  }
+  return {
+    miles: metricsQueryResult.data!,
+    seconds: secondsQueryResult.data!,
+    isPending: false,
+    error: null,
+  };
 }
