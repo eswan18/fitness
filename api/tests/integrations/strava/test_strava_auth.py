6a823662d64cb10db8255e6148feb029bd7faf45
Ethan Swan <ethanpswan@gmail.com>
2025-11-30 21:13:04 -0600

    Get new test working.

--- api/tests/integrations/strava/test_strava_auth.py
+++ api/tests/integrations/strava/test_strava_auth.py
@@ -1,9 +1,16 @@
+import pytest
+
 from fitness.integrations.strava.auth import build_oauth_authorize_url
 
 
-def test_build_oauth_authorize_url():
-    url = build_oauth_authorize_url("https://example.com")
+def test_build_oauth_authorize_url(monkeypatch):
+    monkeypatch.setattr("fitness.integrations.strava.auth.CLIENT_ID", "123")
+    monkeypatch.setattr(
+        "fitness.integrations.strava.auth.OAUTH_URL",
+        "https://www.fakestrava.com/oauth/authorize",
+    )
+    url = build_oauth_authorize_url("https://examplecallback.com")
     assert (
         url
-        == "https://www.strava.com/oauth/authorize?client_id=test_client_id&redirect_uri=https%3A%2F%2Fexample.com&scope=activity%3Aread_all&response_type=code"
+        == "https://www.fakestrava.com/oauth/authorize?client_id=123&redirect_uri=https%3A%2F%2Fexamplecallback.com&scope=activity%3Aread_all&response_type=code"
     )
